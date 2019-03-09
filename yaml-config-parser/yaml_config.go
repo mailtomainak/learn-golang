81349cf3c731f302179cb00e4734ab773a939b98
mailtomainak <mailtomainak@gmail.com>
2019-03-09 22:27:00 +0530

    Initial Checkin

---
+++ yaml-config-parser/yaml_config.go
@@ -0,0 +1,18 @@
+/* Program that parses a YAML file and prints */
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/kylelemons/go-gypsy/yaml"
+)
+
+func main() {
+	config, err := yaml.ReadFile("config.yaml")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(config.Get("path"))
+	fmt.Println(config.GetBool("enabled"))
+}
