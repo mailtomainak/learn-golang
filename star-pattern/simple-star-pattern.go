ddddb135b3733109da716b143db35bae773c9378
mailtomainak <mailtomainak@gmail.com>
2019-03-10 12:39:24 +0530

    Draw a simple Star Pattern to Understand Loops in Go

---
+++ star-pattern/simple-star-pattern.go
@@ -0,0 +1,16 @@
+/*
+Draw a simple Star Pattern to Understand Loops in Go
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 5; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+}
